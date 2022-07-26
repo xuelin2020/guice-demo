55c20abe71d27072dc71c695904a31daa6d1b0f3
Selvin Xue <xuexuedada@icloud.com>
2022-07-26 11:48:11 +0800

    使用内嵌测试的方式，将子任务分为三大块

---
+++ src/test/java/com/tdd/di/ContainerTest.java
@@ -0,0 +1,28 @@
+package com.tdd.di;
+
+import org.junit.jupiter.api.Nested;
+
+public class ContainerTest {
+
+
+
+
+    @Nested
+    public class ComponentConstruction{
+
+    }
+
+
+    @Nested
+    public class DependenciesSelection{
+
+    }
+
+
+    @Nested
+    public class LifecycleManagement{
+
+
+    }
+
+}
