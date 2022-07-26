639ed85a56f618730946d2de47ded22b32282b87
Selvin Xue <xuexuedada@icloud.com>
2022-07-26 13:35:22 +0800

    修改实现，使测试通过。

--- src/main/java/com/tdd/di/Context.java
+++ src/main/java/com/tdd/di/Context.java
@@ -1,13 +1,18 @@
 package com.tdd.di;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Context {
+
+    private Map<Class<?>, Object> components = new HashMap<>();
+
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-
-
+        components.put(type, instance);
     }
 
 
-    public <ComponentType> ComponentType get(Class<ComponentType> componentClass) {
-        return null;
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) components.get(type);
     }
 }
