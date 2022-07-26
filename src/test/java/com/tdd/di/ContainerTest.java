b260f69cce1dac0877291379b65b2639ab71756e
Selvin Xue <xuexuedada@icloud.com>
2022-07-26 11:51:16 +0800

    完善待办任务列表（"TODO"内嵌在测试代码块中）

--- src/test/java/com/tdd/di/ContainerTest.java
+++ src/test/java/com/tdd/di/ContainerTest.java
@@ -4,11 +4,28 @@ import org.junit.jupiter.api.Nested;
 
 public class ContainerTest {
 
-
-
-
     @Nested
     public class ComponentConstruction{
+        //TODO: instance
+        //TODO: abstract class
+        //TODO: interface
+
+        @Nested
+        public class ConstructorInjection{
+            //TODO: No args constructor
+            //TODO: with dependencies
+            //TODO: A -> B -> C
+        }
+
+        @Nested
+        public class FieldInjection{
+
+        }
+
+        @Nested
+        public class MethodInjection{
+
+        }
 
     }
 
