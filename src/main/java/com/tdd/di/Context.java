adf959c25e7f81a3c7cbe4d4b342c87c83045016
Selvin Xue <xuexuedada@icloud.com>
2022-07-28 08:09:27 +0800

    refactor.

--- src/main/java/com/tdd/di/Context.java
+++ src/main/java/com/tdd/di/Context.java
@@ -6,32 +6,27 @@ import java.util.Map;
 
 public class Context {
 
-    private Map<Class<?>, Object> components = new HashMap<>();
     private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
-
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
+    public <ComponentType, ComponentImplementation extends ComponentType>
+    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
+        componentImplementations.put(type, implementation);
+    }
 
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type))
-            return (ComponentType) components.get(type);
+        if (providers.containsKey(type))
+            return (ComponentType) providers.get(type).get();
 
         Class<?> implemetation = componentImplementations.get(type);
-
         try {
             return (ComponentType) implemetation.getConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-    }
-
-    public <ComponentType, ComponentImplementation extends ComponentType> void bind
-            (Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementations.put(type, implementation);
     }
 }
