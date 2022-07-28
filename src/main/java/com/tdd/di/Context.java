1a0463a1a153ea2dcf843c3c1471ae59603ffbc1
Selvin Xue <xuexuedada@icloud.com>
2022-07-28 11:17:04 +0800

    调整代码，使测试通过。

--- src/main/java/com/tdd/di/Context.java
+++ src/main/java/com/tdd/di/Context.java
@@ -1,9 +1,11 @@
 package com.tdd.di;
 
+import javax.inject.Inject;
 import javax.inject.Provider;
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
 
@@ -19,12 +21,28 @@ public class Context {
     void bind(Class<Type> type, Class<Implementation> implementation) {
         providers.put(type, (Provider<Type>) () -> {
             try {
-                Constructor<Implementation> injectConstructor = implementation.getConstructor();
+                Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
 
                 Object[] dependencies = stream(injectConstructor.getParameters()).map(p -> get(p.getType())).toArray(Object[]::new);
 
                 return (Type) injectConstructor.newInstance(dependencies);
             } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) throws NoSuchMethodException {
+
+
+        Stream<Constructor<?>> injectContructors = stream(implementation.getConstructors()).filter(
+                c -> c.isAnnotationPresent(Inject.class)
+        );
+
+        return (Constructor<Type>) injectContructors.findFirst().orElseGet(() -> {
+            try {
+                return implementation.getConstructor();
+            } catch (NoSuchMethodException e) {
                 throw new RuntimeException(e);
             }
         });
