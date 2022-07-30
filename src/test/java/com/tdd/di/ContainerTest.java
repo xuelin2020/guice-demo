93bd4e1c2d7a08ed5b627667ba853ced3b882d63
Selvin Xue <xuexuedada@icloud.com>
2022-07-30 20:12:47 +0800

    refactor.

--- src/test/java/com/tdd/di/ContainerTest.java
+++ src/test/java/com/tdd/di/ContainerTest.java
@@ -101,6 +101,15 @@ public class ContainerTest {
         }
 
         @Test
+        void should_throw_exception_if_transitive_dependency_not_found() {
+            context.bind(Component.class, ComponentWithInjectConstructor.class);
+            context.bind(Dependency.class, DependencyWithInjectConstructor.class);
+
+            DependencyNotFoundException exception = assertThrows(DependencyNotFoundException.class, () -> context.get(Component.class));
+            assertEquals(String.class, exception.getDependency());
+        }
+
+        @Test
         void should_throw_exception_if_cyclic_dependencies_found() {
             context.bind(Component.class, ComponentWithInjectConstructor.class);
             context.bind(Dependency.class, DependencyDependedOnComponent.class);
